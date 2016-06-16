0093563d69e31eb1bf14dbaad393f0985e96ffad
Aleix Bueso <aleix.bueso@gmail.com>
2016-06-16 17:18:21 +0200

    First commit

---
+++ Hexa Quest/Assets/BossScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BossScript : MonoBehaviour {
+    public int HP;
+    public int Damage;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Hexa Quest/Assets/PlayerScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+    public uint Damage;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.Translate(new Vector3(15, 0, 17.32f / 2));
+            transform.Rotate(new Vector3(0, -60, 0));
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.Rotate(new Vector3(0, 60, 0));
+            transform.Translate(new Vector3(- 15, 0, - 17.32f / 2));
+        }
+
+        else
+        {
+            
+        }
+
+    }
+}
