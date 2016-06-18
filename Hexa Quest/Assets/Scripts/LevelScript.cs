39cc9cc805b4f80ba73241674fc7315804897b80
Aleix Bueso <aleix.bueso@gmail.com>
2016-06-18 13:41:42 +0200

    Text, movement and bar improvements

--- Hexa Quest/Assets/Scripts/LevelScript.cs
+++ Hexa Quest/Assets/Scripts/LevelScript.cs
@@ -2,7 +2,8 @@
 using UnityEngine.UI;
 using System.Collections;
 
-public class LevelScript : MonoBehaviour {
+public class LevelScript : MonoBehaviour
+{
 
     public float AttackCD;
     float AttackTime;
@@ -13,47 +14,62 @@ public class LevelScript : MonoBehaviour {
     public GameObject Boss;
     public Slider HealthBar;
     public Text CurrentHealth;
+    public GameObject tile_1;
 
-    
 
-	// Use this for initialization
-	void Start ()
+
+    // Use this for initialization
+    void Start()
     {
         FlotatingDamageControllerScript.Initialize();
         HealthBar.value = BossHP;
         AttackTime = AttackCD;
-	}
-	
-	// Update is called once per frame
-	void Update ()
+    }
+
+    // Update is called once per frame
+    void Update()
     {
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
+            //iTween.MoveTo(Player, posició seguent tile, 0.5f);
+            //iTween.RotateTo(Player, new Vector3(0, -60, 0), 0.5f);
+            
             Player.transform.Translate(new Vector3(15, 0, 17.32f / 2));
             Player.transform.Rotate(new Vector3(0, -60, 0));
+
             AttackTime = AttackCD;
         }
 
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
+            //iTween.RotateTo(Player, new Vector3(0, 60, 0), 0.5f);
+            //iTween.MoveTo(Player, posició anterior tile, 0.5f);
+
             Player.transform.Rotate(new Vector3(0, 60, 0));
             Player.transform.Translate(new Vector3(-15, 0, -17.32f / 2));
+
             AttackTime = AttackCD;
         }
 
         else
         {
             AttackTime -= Time.deltaTime;
-            if (AttackTime < 0)
+            if (AttackTime < 0 || Input.GetKeyDown(KeyCode.Space))
             {
-                FlotatingDamageControllerScript.CreateFloatingText(PlayerDamage.ToString(), Boss.transform);
-                HealthBar.value -= PlayerDamage;
-                AttackTime = AttackCD;
-
+                if (Boss)
+                {
+                    FlotatingDamageControllerScript.CreateFloatingText(PlayerDamage.ToString(), Boss.transform);
+                    HealthBar.value -= PlayerDamage;
+                    AttackTime = AttackCD;
+                }
             }
         }
+
         CurrentHealth.text = HealthBar.value + " / " + BossHP + " HP";
+
+        if(HealthBar.value <= 0)
+        {
+            Destroy(Boss.gameObject);
+        }
     }
 }
-
-
