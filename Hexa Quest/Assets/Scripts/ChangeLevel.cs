f2613f85177094d57ad0b692ca8e8069c77da0a0
Jan Daniel Naftanaila <jannafta@gmail.com>
2016-07-10 18:54:04 +0200

    Flow Chart Added

---
+++ Hexa Quest/Assets/Scripts/ChangeLevel.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ChangeLevel : MonoBehaviour {
+
+	public void change_Level_MenuScene() {
+		
+		SceneManager.LoadScene ("MenuScene");
+	}
+
+	public void change_Level_StartScene() {
+
+		SceneManager.LoadScene ("StartScene");
+	}
+
+	public void change_Level_Options() {
+
+		SceneManager.LoadScene ("Options");
+	}
+
+	public void change_Level_About() {
+
+		SceneManager.LoadScene ("About");
+	}
+
+	public void change_Level_GameScene() {
+
+		SceneManager.LoadScene ("GameScene");
+	}
+
+	public void change_Level_Shop() {
+
+		SceneManager.LoadScene ("Shop");
+	}
+
+
+	public void change_Level_SelectBoss() {
+
+		SceneManager.LoadScene ("SelectBoss");
+	}
+
+	public void change_Level_Boss1() {
+		
+		SceneManager.LoadScene ("Boss1");
+	}
+
+	public void change_Level_Boss2() {
+
+		SceneManager.LoadScene ("Boss2");
+	}
+
+	public void change_Level_Boss3() {
+
+		SceneManager.LoadScene ("Boss3");
+	}
+
+	public void change_Level_Boss4() {
+
+		SceneManager.LoadScene ("Boss4");
+	}
+
+	public void change_Level_Boss5() {
+
+		SceneManager.LoadScene ("Boss5");
+	}
+
+	public void change_Level_Boss6() {
+
+		SceneManager.LoadScene ("Boss6");
+	}
+
+	public void change_Level_Boss7() {
+
+		SceneManager.LoadScene ("Boss7");
+	}
+
+	public void change_Level_Boss8() {
+
+		SceneManager.LoadScene ("Boss8");
+	}
+
+	public void change_Level_Boss9() {
+
+		SceneManager.LoadScene ("Boss9");
+	}
+
+	public void change_Level_Boss10() {
+
+		SceneManager.LoadScene ("Boss10");
+	}
+}
